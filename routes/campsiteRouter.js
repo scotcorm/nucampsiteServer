7f94854475928fcadb3c7f6f56fdbd672558217e
Scott Cormier <chicormier@gmail.com>
2024-02-15 16:09:34 -0600

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,6 +8,7 @@ campsiteRouter
   .route('/')
   .get((req, res, next) => {
     Campsite.find()
+      .populate('comments.author')
       .then((campsites) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -45,6 +46,7 @@ campsiteRouter
   .route('/:campsiteId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -52,6 +54,7 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
+
   .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.setHeader('Content-Type', 'text/plain');
@@ -88,6 +91,7 @@ campsiteRouter
   .route('/:campsiteId/comments')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         if (campsite) {
           res.statusCode = 200;
@@ -105,6 +109,7 @@ campsiteRouter
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
+          req.body.author = req.user._id;
           campsite.comments.push(req.body);
           campsite
             .save()
@@ -157,6 +162,7 @@ campsiteRouter
   .route('/:campsiteId/comments/:commentId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
           res.statusCode = 200;
