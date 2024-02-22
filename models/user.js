e85bd4b7ab693077b5789ed42e87e70ebd52661a
Scott Cormier <chicormier@gmail.com>
2024-02-21 19:50:02 -0600

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  facebookId: String,
   firstname: {
     type: String,
     default: '',
